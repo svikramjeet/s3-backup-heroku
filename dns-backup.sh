72b2c8f0cd1bd6139cb673793768383072e438f4
Surinder Singh <sssurii.dev@gmail.com>
2024-01-18 10:48:26 +0530

    Update dns-backup.sh
    Fix S3 upload command for ENV backup - Bucket variable was incorrect.

--- dns-backup.sh
+++ dns-backup.sh
@@ -18,8 +18,7 @@ dig $1 any +noall +answer | gzip >  /tmp/"${FNAME}_${FILENAME}".gz
 EXPIRATION_DATE=$(date -d "$EXPIRATION days" +"%Y-%m-%dT%H:%M:%SZ")
 
 printf "${Green}Move dns-backup to AWS${EC}"
-AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_BACKUP AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY_BACKUP AWS_DEFAULT_REGION=$AWS_REGION_BACKUP /app/vendor/bin/aws s3 cp /tmp/"${FNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$FNAME/"${FNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
-
+AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_BACKUP AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY_BACKUP AWS_DEFAULT_REGION=$AWS_REGION_BACKUP /app/vendor/bin/aws s3 cp /tmp/"${FNAME}_${FILENAME}".gz s3://$AWS_BUCKET_S3_BACKUP/$FNAME/"${FNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
 
 # cleaning after all
 rm -rf /tmp/"${FNAME}_${FILENAME}".gz
